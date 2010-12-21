18bee520f12bd9b2c1c3c5f3f9dce66eda36e662
johan.vandenbrande@gmail.com <johan.vandenbrande@gmail.com@523fe40f-9772-39ae-8ccc-b213c7d40fcc>
2010-12-21 11:53:55 +0000

    First public release.

---
+++ trunk/README.txt
@@ -0,0 +1,16 @@
+PyVernier - Draw Vernier Scales with Python
+===========================================
+
+Summary
+-------
+
+This Python package draws vernier scales, as found on callipers. You can find more information about vernier
+scales behind the following link: http://en.wikipedia.org/wiki/Vernier_scale
+
+Usage
+-----
+
+Have a look at the examples in the 'examples' directory, they should be fairly easy to understand.
+
+I wrote this package so I could use a vernier scale on my center of gravity gauge, to increase the resolution
+of the readout a bit.
---
+++ trunk/examples/example_4.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert( 0, ".." )
+
+from pyvernier import vernier
+
+config = {
+            'filename': 'example_4.pdf',        # Name of the file
+            'main_scale_size': 50,              # Size of the main scale in mm
+            'vernier_size': 4,                  # Width between two ticks on the vernier scale
+            'vernier_resolution': 0.1,          # Resolution for the vernier scale (0.1 or 0.05 are sane values) 
+            'major_tick_size': 5,               # Major tick size for the main scale
+            'minor_tick_size': 3,               # Minor tick size for the main scale
+            'vernier_major_tick_size': 4,       # Major tick size for the vernier scale
+            'vernier_minor_tick_size': 2,       # Minor tick size for the vernier scale
+            'line_width': 0.1,                  # Width of all lines
+            'text_margin': 0.5,                 # Margin around the text
+            'text_size': 3,                     # Size of the text
+            'gap': 2,                           # Gap between main and vernier scale
+            'font_name': 'CMU Concrete',        # Name of the font - Those classic TeX fonts!
+            'color': '(1.0, 1.0, 1.0)',         # Color of the lines
+            'background_color': '( 0, 0, 0 )'   # Background color
+    }
+
+vernier(config)
